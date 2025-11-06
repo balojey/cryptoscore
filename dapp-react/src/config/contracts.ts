67a973b7a28e0c39eccba9a55deb2de8ec2066f8
balo <balo@alexandria.pc>
2025-11-06 16:56:02 +0100

    First commit

---
+++ dapp-react/src/config/contracts.ts
@@ -0,0 +1,5 @@
+// Contract address for Passet Hub Testnet
+export const MESSAGE_BOARD_ADDRESS = '0xaB7B4c595d3cE8C85e16DA86630f2fc223B05057'
+
+// Export ABI
+export { abi as MessageBoardABI } from '../../abi/MessageBoard.json'
