fcfe9cee9a614152a6678348c3640d10f80f4252
balo <balo@alexandria.pc>
2025-11-08 10:41:06 +0100

    Hot fix

--- dapp-react/src/config/contracts.ts
+++ dapp-react/src/config/contracts.ts
@@ -2,4 +2,4 @@
 export const MESSAGE_BOARD_ADDRESS = '0xaB7B4c595d3cE8C85e16DA86630f2fc223B05057'
 
 // Export ABI
-export { abi as MessageBoardABI } from '../../abi/MessageBoard.json'
+export { abi as CryptoScoreMarketsABI } from '../../abi/CryptoScoreMarkets.json'
