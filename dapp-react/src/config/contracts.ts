5c5439ce7a3bee7093faa438f1f01782c3df25ec
balo <98645847+balojey@users.noreply.github.com>
2025-11-09 13:26:58 +0000

    Smart contract deployed

--- dapp-react/src/config/contracts.ts
+++ dapp-react/src/config/contracts.ts
@@ -1,5 +1,6 @@
 // Contract address for Passet Hub Testnet
 export const MESSAGE_BOARD_ADDRESS = '0xaB7B4c595d3cE8C85e16DA86630f2fc223B05057'
+export const CRYPTO_SCORE_ADDRESS = '0xc775bF567D67018dfFac4E89a7Cf10f0EDd0Be93'
 
 // Export ABI
-export { abi as CryptoScoreMarketsABI } from '../../abi/CryptoScoreMarkets.json'
+export { abi as CryptoScoreMarketsABI } from '../../abi/CryptoScoreFactory.json'
