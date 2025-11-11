6f558817aec0eb013f92149dde7db597f34b9574
balo <98645847+balojey@users.noreply.github.com>
2025-11-11 08:35:52 +0000

    Contract ABI fix

--- dapp-react/src/config/contracts.ts
+++ dapp-react/src/config/contracts.ts
@@ -1,7 +1,9 @@
 // Contract address for Passet Hub Testnet
 export const MESSAGE_BOARD_ADDRESS = '0xaB7B4c595d3cE8C85e16DA86630f2fc223B05057'
-export const CRYPTO_SCORE_FACTORY_ADDRESS = '0xbeC6419cD931e29ef41157fe24C6928a0C952f0b'
-export const CRYPTO_SCORE_DASHBOARD_ADDRESS = '0x55027d3dBBcEA0327eF73eFd74ba0Af42A13A966'
+export const CRYPTO_SCORE_FACTORY_ADDRESS = '0x02e8910B3B89690d4aeC9fcC0Ae2cD16fB6A4828'
+export const CRYPTO_SCORE_DASHBOARD_ADDRESS = '0x484242986F57dFcA98EeC2C78427931C63F1C4ce'
 
 // Export ABI
-export { abi as CryptoScoreMarketsABI } from '../../abi/CryptoScoreFactory.json'
+export { abi as CryptoScoreFactoryABI } from '../../abi/CryptoScoreFactory.json'
+export { abi as CryptoScoreDashboardABI } from '../../abi/CryptoScoreDashboard.json'
+export { abi as CryptoScoreMarketABI } from '../../abi/CryptoScoreMarket.json'
